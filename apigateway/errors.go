58763357873c4b0984e60780cf402c84a0795148
xier <xier.kokis@gmail.com>
2018-12-20 16:12:14 +0700

    update error

--- apigateway/errors.go
+++ apigateway/errors.go
@@ -1,9 +1,17 @@
 package apigateway
 
 import (
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/onedaycat/errors"
 )
 
 var (
 	ErrorUnmarshalJSON = errors.BadRequest("3000", "Unable unmarshal json")
 )
+
+func ErrorUnmarshalJSONResponse() *events.APIGatewayProxyResponse {
+	return &events.APIGatewayProxyResponse{
+		StatusCode: ErrorUnmarshalJSON.Status,
+		Body:       ErrorUnmarshalJSON.Error(),
+	}
+}
